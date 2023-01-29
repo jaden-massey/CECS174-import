ac39b913e6096d27cc552211547e7d5b3580ac8e
jaden-massey <122401895+jaden-massey@users.noreply.github.com>
2023-01-28 18:04:19 -0700

    more homework

--- byui/cse-110-03Prove.py
+++ byui/cse-110-03Prove.py
@@ -6,6 +6,44 @@ tax = float(input('What is the sales tax rate? '))
 
 subtotal = cMeal * countChild + mMeal * countAdult
 taxAmt = (tax / 100) * subtotal
-total = taxAmt + subtotal
 
-print(f'{subtotal:.2f}')
+print(f'\nSubtotal: ${subtotal:.2f}')
+print(f'Sales Tax: ${taxAmt:.2f}')
+
+
+def tip():
+    tipOp = int(input(f'''
+Would you like to offer a tip?
+0 --- No Tip
+1 --- 12% ({subtotal * 0.12:.2f})
+2 --- 15% ({subtotal * 0.15:.2f})
+3 --- 18% ({subtotal * 0.18:.2f})
+\n'''))
+    flag = True
+    while flag:
+        if tipOp == 0:
+            return 0
+        elif tipOp == 1:
+            return subtotal * 0.12
+        elif tipOp == 2:
+            return subtotal * 0.15
+        elif tipOp == 3:
+            return subtotal * 0.18
+        else:
+            print('Error: Invalid Input')
+            tipOp = int(input(f'''
+Would you like to offer a tip?
+0 --- No Tip
+1 --- 12% ({subtotal * 0.12:.2f})
+2 --- 15% ({subtotal * 0.15:.2f})
+3 --- 18% ({subtotal * 0.18:.2f})
+\n'''))
+
+
+total = taxAmt + subtotal + tip()
+print(f'Total: ${total:.2f}\n')
+
+payAmt = float(input('What is the payment amount? '))
+change = payAmt - total
+
+print(f'Change: ${change:.2f}')
